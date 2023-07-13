e1d409ad8d29fecaf6fcc363b0be127a84fb5cd6
Vishal Yadav <87231229+aham-vishal-yadav@users.noreply.github.com>
2023-07-14 03:09:13 +0530

    Update octoprime_tires.py

--- tyre/octoprime_tires.py
+++ tyre/octoprime_tires.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+from lyft.tire.tire import Tire
+
+
+# For concrete/child/subclass "OctoprimeTires" to "'instantiate' the 'abstract parent/base class 'Tire,'' its 'parent
+# class' ''must' be called.'"
+
+
+class OctoprimeTires(Tire):
+    def __init__(self, x1, y1, x2, y2):
+        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+        self.new_tire_wear_sensors = np.asfarray([self.x1, self.y1, self.x2, self.y2])
+
+    def needs_service(self):
+        return sum(self.new_tire_wear_sensors) >= 3
